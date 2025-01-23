594b9b29ecb630a6949bd14170eb7972ad11cff4
GithubGitPro <90504178+GithubGitPro@users.noreply.github.com>
2025-01-23 16:40:42 +0100

    formatting changes

--- main.c
+++ main.c
@@ -1,28 +1,24 @@
 #include <gtk/gtk.h>
+#include <stdio.h>
 
-static void
-activate (GtkApplication* app,
-          gpointer        user_data)
-{
-    GtkWidget *window;
+GtkApplication *app;
+int status;
+GtkWidget *window;
+const int WIDTH = 400;
+const int HEIGHT = 400;
 
+static void activate (GtkApplication* app, gpointer user_data) {
     window = gtk_application_window_new (app);
-    gtk_window_set_title (GTK_WINDOW (window), "Window");
-    gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
-    gtk_widget_show_all (window);
+    gtk_window_set_title(GTK_WINDOW(window), "Paint 2 (but worse)");
+    gtk_window_set_default_size(GTK_WINDOW(window), WIDTH, HEIGHT);
+    gtk_widget_show_all(window);
 }
 
-int
-main (int    argc,
-      char **argv)
-{
-    GtkApplication *app;
-    int status;
-
-    app = gtk_application_new ("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
-    status = g_application_run (G_APPLICATION (app), argc, argv);
-    g_object_unref (app);
+int main (const int argc, char **argv) {
+    app = gtk_application_new("org.gtk.example", G_APPLICATION_DEFAULT_FLAGS);
+    g_signal_connect(app, "activate", G_CALLBACK (activate), NULL);
+    status = g_application_run(G_APPLICATION (app), argc, argv);
+    g_object_unref(app);
 
     return status;
 }
